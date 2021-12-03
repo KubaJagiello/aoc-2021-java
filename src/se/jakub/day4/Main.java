c25b6a1ac10a4ef5ccc78236209503abb4dd1edb
Jakub <kuba@cs.umu.se>
2021-12-03 23:13:28 +0100

    day4

--- src/se/jakub/day4/Main.java
+++ src/se/jakub/day4/Main.java
@@ -1,2 +1,23 @@
-package se.jakub.day4;public class Main {
+package se.jakub.day4;
+
+import static java.util.stream.Collectors.toList;
+import static se.jakub.reader.DayReader.inputReader;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(partOne()); //
+        System.out.println(partTwo()); //
+    }
+
+    private static int partOne() {
+        var l = inputReader(4, 1).collect(toList());
+
+        return 0;
+    }
+
+    private static int partTwo() {
+        var l = inputReader(4, 2).collect(toList());
+
+        return 0;
+    }
 }
