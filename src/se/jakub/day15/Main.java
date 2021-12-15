005d46cf3e57ae5807b234f7fb72f1134e19799c
Jakub Jagiello <jakub.jagiello@globalconnect.se>
2021-12-15 12:22:43 +0100

    runs faster now

--- src/se/jakub/day15/Main.java
+++ src/se/jakub/day15/Main.java
@@ -1,134 +1,67 @@
 package se.jakub.day15;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Objects;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
+import static java.lang.Integer.parseInt;
+import static java.lang.String.valueOf;
 import static java.util.stream.Collectors.toList;
+import static java.util.stream.IntStream.range;
 import static se.jakub.reader.DayReader.inputReader;
 
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(partOne()); // 652
-        System.out.println(partTwo()); // 2938
+        System.out.println(partOneAndTwo(1, 1)); // 652
+        System.out.println(partOneAndTwo(5, 5)); // 2938
     }
 
-    private static int partOne() {
+    private static int partOneAndTwo(int xTiles, int yTiles) {
         var m = inputReader(15, 2).collect(toList());
+        var xMoves = new int[]{0, 0, 1, -1};
+        var yMoves = new int[]{1, -1, 0, 0};
 
-        var moves = List.of(
-                new Point(1, 0),
-                new Point(0, 1),
-                new Point(0, -1)
-        );
-
-        var queue = new LinkedList<Point>();
-        var cost = new HashMap<Point, Integer>();
-        queue.addFirst(new Point(0, 0));
-
-        while (!queue.isEmpty()) {
-            var p = queue.removeFirst();
-
-            for (var move : moves) {
-                var newX = p.x + move.x;
-                var newY = p.y + move.y;
-
-                if (newX >= 0 && newY >= 0 && newX < m.get(0).length() && newY < m.size()) {
-                    var currentCost = cost.getOrDefault(new Point(newX, newY), Integer.MAX_VALUE);
-                    var n = Integer.parseInt(String.valueOf(m.get(newY).charAt(newX)));
-
-                    if (currentCost > (p.pathCost + n)) {
-                        var newP = new Point(newX, newY);
-                        newP.pathCost = p.pathCost + n;
-                        cost.put(newP, newP.pathCost);
-                        queue.addLast(newP);
-                    }
-                }
-            }
-        }
-
-        var maxX = m.get(0).length() - 1;
-        var maxY = m.size() - 1;
-
-        return cost.get(new Point(maxX, maxY));
-    }
-
-    private static int partTwo() {
-        var m = inputReader(15, 2).collect(toList());
-
-        var moves = List.of(
-                new Point(1, 0),
-                new Point(0, 1),
-                new Point(0, -1),
-                new Point(-1, 0)
-        );
-
-        var queue = new LinkedList<Point>();
-        var cost = new HashMap<Point, Integer>();
-        queue.addFirst(new Point(0, 0));
+        var queue = new PriorityQueue<>(Comparator.comparingInt(Point::pathCost));
+        queue.add(new Point(0, 0, 0));
 
         var actualWidth = m.get(0).length();
         var actualHeight = m.size();
+        var width = actualWidth * xTiles;
+        var height = actualHeight * yTiles;
 
-        var width = m.get(0).length() * 5;
-        var height = m.size() * 5;
+        int[][] cost = new int[height][width];
+        range(0, height).forEach(i -> Arrays.fill(cost[i], Integer.MAX_VALUE));
 
         while (!queue.isEmpty()) {
-            var p = queue.removeFirst();
+            var p = queue.remove();
 
-            for (var move : moves) {
-                var newX = p.x + move.x;
-                var newY = p.y + move.y;
+            if (p.x() == width - 1 && p.y() == height - 1) {
+                break;
+            }
+
+            for (int i = 0; i < xMoves.length; i++) {
+                var newX = p.x() + xMoves[i];
+                var newY = p.y() + yMoves[i];
 
                 if (newX >= 0 && newY >= 0 && newX < width && newY < height) {
-                    var wrappedX = newX % actualWidth;
-                    var wrappedY = newY % actualHeight;
+                    var n = parseInt(valueOf(m.get(newY % actualHeight).charAt(newX % actualWidth)));
+                    n += newX / actualWidth + newY / actualHeight;
 
-                    var currentCost = cost.getOrDefault(new Point(newX, newY), Integer.MAX_VALUE);
-                    var n = Integer.parseInt(String.valueOf(m.get(wrappedY).charAt(wrappedX)));
-
-                    n += newX / actualWidth;
-                    n += newY / actualHeight;
-
-                    while (n > 9) {
-                        n -= 9;
+                    if (n > 9) {
+                        n = (n % 10) + 1;
                     }
 
-                    if (currentCost > (p.pathCost + n)) {
-                        var newP = new Point(newX, newY);
-                        newP.pathCost = p.pathCost + n;
-                        cost.put(newP, newP.pathCost);
-                        queue.addLast(newP);
+                    if (cost[newY][newX] > (p.pathCost() + n)) {
+                        queue.add(new Point(newX, newY, p.pathCost() + n));
+                        cost[newY][newX] = p.pathCost() + n;
                     }
                 }
             }
         }
-        return cost.get(new Point(width - 1, height - 1));
+        return cost[height - 1][width - 1];
     }
 }
 
-class Point {
-    int x;
-    int y;
-    int pathCost;
-
-    public Point(int x, int y) {
-        this.x = x;
-        this.y = y;
-        pathCost = 0;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        Point point = (Point) o;
-        return x == point.x && y == point.y;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(x, y);
-    }
+record Point(int x, int y, int pathCost) {
 }
